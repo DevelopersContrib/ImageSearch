4c25a60276070b2061437e262a5b1bd472c3e570
rownone <row_none@yahoo.com>
2015-07-30 15:07:11 +0800

    widget page

---
+++ widget.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Image Search Widget</title>
+<script>
+function clickcallback(img){
+	alert(img);
+}
+</script>
+</head>
+<body>
+ <script src="http://tools.contrib.com/ImageSearch?clickcallback=clickcallback&containerid=test"></script>
+<div id="test"></div>
+</body>
+</html>
